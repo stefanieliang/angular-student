18b90f2a37997690cc5ec19d30ab3589fb47eb4e
stefanieliang <751179350@qq.com>
2018-12-04 16:15:56 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+
+// 配置路由
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+];
+
+@NgModule({
+  declarations: [ // 声明组件
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule, // 使用模板驱动表单
+    RouterModule.forRoot(routes),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {LoginUser} from '../login-user';
+import {User, UserService} from '../user.service';
+import {Result} from '../common/result';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  model: LoginUser;
+
+  constructor(private userService: UserService) {
+    this.model = new LoginUser();
+  }
+
+  login() {
+    // 发送请求
+    this.userService.login(this.model).subscribe(
+      (result: Result<User>) => {
+        if (result.success) {
+          alert('登陆成功！');
+        } else {
+          alert('登录失败！');
+        }
+      },
+      (error) => {
+        console.log(error);
+        alert('登录失败');
+      }
+    );
+  }
+
+}
